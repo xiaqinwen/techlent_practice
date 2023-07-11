2587b6eda209ecb0658f76404fe43d22d5585dfd
xiaqinwen11 <xiaqinwen11@#163.com>
2023-07-10 22:58:46 -0400

    add new files

---
+++ app/main.py
@@ -0,0 +1,4 @@
+from src.calculator import Calculator
+cal = Calculator()
+
+print(cal.add(1,2))
---
+++ app/src/calculator.py
@@ -0,0 +1,22 @@
+class Calculator():
+    def add(self, num1, num2):
+        #return num1+num2
+        return 2
+
+    def substract(self, num1, num2):
+        return num1-num2
+
+    def multiply(self, num1, num2):
+        return num1*num2
+
+    def divide(self, num1, num2):
+        if num2 == 0:
+            return 0
+        return num1 / num2
+
+if __name__ == "__main__":
+    calculator = Calculator()
+    print(calculator.add(1,3))
+    print(calculator.substract(3, 5))
+    print(calculator.divide(2,0))
+    print(calculator.divide(4,2))
